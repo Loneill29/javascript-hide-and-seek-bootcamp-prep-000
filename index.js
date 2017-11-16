2942dfdfa4207e035e8b66e3c3dee5c36722d0aa
Linda ONeill <oneill.linda21@gmail.com>
2017-11-16 18:08:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
+function deepestChild(){
+  var deepChild = document.getElementById('grand-node').querySelectorAll('div')
+  return deepChild[deepChild.length-1]}
